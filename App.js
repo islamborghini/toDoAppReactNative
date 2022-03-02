68444a37852f42094fc8ecb38fd571159c16a014
islamborghini <islamassanov66@gmail.com>
2022-03-02 15:24:28 +0600

    v 1.0 toDoApp from tutorial

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, KeyboardAvoidingView,  TouchableOpacity } from 'react-native';
+import Note from './components/Note';
 export default function App() {
+  const[note, setNote] = useState();
+  const [noteItems, setNoteItems] = useState([]);
+
+  const handleAddNote = () =>{
+    setNoteItems([...noteItems, note])
+    setNote(null);
+  }
+
+  const completeNote = (index) =>{
+    let itemsCopy = [...noteItems];
+    itemsCopy.splice(index, 1);
+    setNoteItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/*Today's tasks*/}
+        <View style={styles.tasksWrapper}>
+            <Text style={styles.sectionTitle}>Your notes</Text>
+            
+            <View style = {styles.items}>
+              {/*This is where the tasks will go*/}
+              {
+                noteItems.map((item, index)=>{
+                  return (
+                    <TouchableOpacity key={index} onPress={()=>completeNote(index)}>
+                        <Note key={index} text={item}></Note>
+                    </TouchableOpacity>
+                  )
+                })
+              }
+            </View>
+        </View>
+        
+        {/*Write a note*/}
+        <KeyboardAvoidingView 
+          behaviour={Platform.OS === "ios"?"padding":"height"}
+          style={styles.writeNoteWrapper}
+          >
+            <TextInput style={styles.input} placeholder={'Write a note'}value = {note} onChangeText={text => setNote(text)}/>
+
+        <TouchableOpacity onPress={()=>handleAddNote()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+        </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -13,8 +57,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+
   },
+  tasksWrapper:{
+    paddingTop:80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items:{
+    marginTop: 30,
+  },
+  writeNoteWrapper:{
+    position:'absolute',
+    bottom:60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems:'center'
+  },
+  input:{
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addWrapper:{
+    width: 60, 
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText:{},
+
 });
